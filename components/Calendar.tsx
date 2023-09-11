8ac1d87a6db2d99d128ec8839aa6eddbfccfa8c2
Eric Nichols <ebn646@gmail.com>
2023-09-11 09:18:51 -0400

    added date changing

--- components/Calendar.tsx
+++ components/Calendar.tsx
@@ -1,8 +1,26 @@
+import { useEffect, useState } from 'react';
 import { Calendar } from './ui/calendar';
 
 const CalendarComponent = () => {
+  const [selectedDay, setSelectedDay] = useState<Date[]>(
+    [
+      "2023-09-11T04:00:00.000Z",
+      //"2023-09-26T04:00:00.000Z"
+    ].map(d => new Date(d))
+  );
+
+  useEffect(() => {
+    console.log(selectedDay)
+  }, [selectedDay])
+  
   return (
-    <Calendar />
+    <div>
+      <Calendar 
+      mode="multiple"
+      selected={selectedDay}
+    />
+    </div>
+
   )
 }
 
