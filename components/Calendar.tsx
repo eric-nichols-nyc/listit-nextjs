a477b640c4662e8bc89f30cfa7e2167f52a56945
Eric Nichols <ebn646@gmail.com>
2023-09-11 20:35:17 -0400

    calendar update

--- components/Calendar.tsx
+++ components/Calendar.tsx
@@ -1,46 +1,41 @@
 import { useCallback, useEffect, useState } from 'react';
 import { Calendar } from './ui/calendar';
 import { useTaskStore } from '@/store/taskStore';
+import Task from './Task';
 import { Checkbox } from './ui/checkbox';
-import { toast } from 'react-toastify';
 
 // Calendar component dynamically and reflects the dates of tasks
-// if a date is selected the list at the bottom updated
 const CalendarComponent = () => {
-  // global state
-  const [tasks, removeTask] = useTaskStore(state => [state.tasks, state.removeTask])
+  const tasks = useTaskStore(state => state.tasks)
 
   // return unique dates from tasks
-  const getUniqueDates = useCallback((arr:Task[]): Date[] => {
-    const dates = arr.map((task:Task) => task.duedate)
+  const getUniqueDates = useCallback((): Date[] => {
+    const dates = tasks.map(task => task.duedate)
     const unique = Array.from(new Set(dates))
     return unique.map(d => new Date(d))
   }, [tasks])
 
-  // when a date is clicked, get items to show for this day 
+  // get items to show for this day
   const getItemsForDay = (day: Date) => {
-    setSelectedDate(day)
     const substring = day.toISOString().substring(0, 10)
     const list = tasks.filter(task => task.duedate.toISOString().substring(0, 10) === substring)
     const unique = Array.from(new Set(list))
     setFilteredDays(unique)
   }
-  // local state
-  const [selectedDays, setSelectedDays] = useState<Date[]>(getUniqueDates(tasks));
-  const [filteredDays, setFilteredDays] = useState<any[]>([]);
-  const [selectedDate, setSelectedDate] = useState<Date | undefined>()
 
+  const [selectedDays, setSelectedDays] = useState<Date[]>(getUniqueDates());
+  const [filteredDays, setFilteredDays] = useState<any[]>([])
   useEffect(() => {
-    // refresh calendar dates when tasks change
-    setSelectedDays(getUniqueDates(tasks))
-    if(selectedDate){
-      // refresh list of projects at bottom of calendar
-      getItemsForDay(selectedDate)
-    }
-  }, [tasks, getUniqueDates, selectedDate])
+    setFilteredDays([] as any[])
+  }, [selectedDays])
+
+  // refresh calendar when tasks change
+  useEffect(() => {
+    setSelectedDays(getUniqueDates())
+  }, [tasks, getUniqueDates])
 
   return (
-    <div className=" border bg-gray-100">
+    <div className=" border-2 bg-gray-100">
       <Calendar
         className="bg-white"
         mode="multiple"
@@ -49,7 +44,7 @@ const CalendarComponent = () => {
       />
       <div>
         {filteredDays.map(day => (
-          <div 
+          <div
             key={day.id}
             className="
             flex 
@@ -58,17 +53,9 @@ const CalendarComponent = () => {
             gap-2 
             items-center
             bg-white
-            shadow-md
             "
           >
-            <Checkbox 
-              onCheckedChange={() => {
-                setTimeout(() => {
-                  toast("1 task completed");
-                  removeTask(day.id)
-                }, 300)
-              }}
-            />
+            <Checkbox />
             <div>
               <div className="text-sm">{day.name}</div>
               <div className='text-xs'>{day.duedate.toDateString()}</div>
